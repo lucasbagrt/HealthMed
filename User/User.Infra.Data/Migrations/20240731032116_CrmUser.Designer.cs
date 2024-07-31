88de295809a9bcc23a5f243f564722c80529d528
Lucas Hanke <lucasghank@hotmail.com>
2024-07-31 00:21:28 -0300

    add migration crm user

--- User/User.Infra.Data/Migrations/20240731032116_CrmUser.Designer.cs
+++ User/User.Infra.Data/Migrations/20240731032116_CrmUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using User.Infra.Data.Context;
 
@@ -11,9 +12,11 @@ using User.Infra.Data.Context;
 namespace User.Infra.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240731032116_CrmUser")]
+    partial class CrmUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
